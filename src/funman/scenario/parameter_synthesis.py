eb2f79a80ce601823b7a941ff87edbb9641a40db
Dan Bryce <dbryce@sift.net>
2023-08-22 01:08:31 +0000

    update imports

--- src/funman/scenario/parameter_synthesis.py
+++ src/funman/scenario/parameter_synthesis.py
@@ -70,7 +70,7 @@ class ParameterSynthesisScenario(AnalysisScenario, BaseModel):
     ]
     query: Union[
         QueryAnd, QueryGE, QueryLE, QueryEncoded, QueryFunction, QueryTrue
-    ]
+    ] = QueryTrue()
     _search: str = "BoxSearch"
     _smt_encoder: Optional[
         Encoder
