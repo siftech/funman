d97a64fe1023d23bb7f3bed179d8d4abc50b10e6
Dan Bryce <dbryce@sift.net>
2023-10-13 15:19:14 +0000

    updated schemas

--- tools/generate-models.sh
+++ tools/generate-models.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-pip install datamodel-code-generator || echo "Didn't install datamodel-code-generator (already installed?)"
-datamodel-codegen --input ~/Model-Representations/petrinet/petrinet_schema.json --input-file-type jsonschema --output src/funman/model/generated_models/petrinet.py
-datamodel-codegen --input ~/Model-Representations/regnet/regnet_schema.json --input-file-type jsonschema --output src/funman/model/generated_models/regnet.py
+pip install datamodel-code-generator[http] || echo "Didn't install datamodel-code-generator (already installed?)"
+# datamodel-codegen --reuse-model --url https://raw.githubusercontent.com/DARPA-ASKEM/Model-Representations/main/petrinet/petrinet_schema.json --input-file-type jsonschema --output-model-type pydantic_v2.BaseModel --output src/funman/model/generated_models/petrinet.py 
+# datamodel-codegen --reuse-model --url https://raw.githubusercontent.com/DARPA-ASKEM/Model-Representations/main/regnet/regnet_schema.json --input-file-type jsonschema --output-model-type pydantic_v2.BaseModel --output src/funman/model/generated_models/regnet.py
+
+datamodel-codegen --reuse-model --input resources/amr/petrinet_schema.json --input-file-type jsonschema --output-model-type pydantic_v2.BaseModel --output src/funman/model/generated_models/petrinet.py 
+datamodel-codegen --reuse-model --input resources/amr/regnet_schema.json --input-file-type jsonschema --output-model-type pydantic_v2.BaseModel --output src/funman/model/generated_models/regnet.py
