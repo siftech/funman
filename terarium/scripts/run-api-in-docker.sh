d14f6da044181c3019b85b7fedca34045b15167c
Daniel Bryce <dbryce@sift.net>
2023-10-21 14:30:37 -0500

    update instructions and script to use ghcr

--- terarium/scripts/run-api-in-docker.sh
+++ terarium/scripts/run-api-in-docker.sh
@@ -4,8 +4,9 @@
 # TARGET_IMAGE=localhost:5000/siftech/funman-api:local
 
 # For public images:
-FUNMAN_VERSION="1.6.0"
-TARGET_IMAGE=ghcr.io/jgladwig/funman-api:$FUNMAN_VERSION
+FUNMAN_VERSION="${FUNMAN_VERSION:-hackathon_fall_2023}"
+TARGET_IMAGE="${TARGET_IMAGE:-ghcr.io/siftech/funman-api:$FUNMAN_VERSION}"
 PLATFORM="${PLATFORM:---platform linux/amd64}"
 
+echo "Running [" docker run $PLATFORM --rm -p 127.0.0.1:8190:8190 --pull always $TARGET_IMAGE " ]"
 docker run $PLATFORM --rm -p 127.0.0.1:8190:8190 --pull always $TARGET_IMAGE
