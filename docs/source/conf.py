089d1023246e9108de9d5c93bd49af844310e8e3
Daniel Bryce <dbryce@sift.net>
2023-10-27 16:30:27 +0000

    update for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "funman"
-copyright = "2022, SIFT"
+copyright = "2023, SIFT"
 author = "SIFT"
 
 from funman import __version__
