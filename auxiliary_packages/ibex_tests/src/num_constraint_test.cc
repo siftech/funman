0c68793f1b144a1742970cd66899fba9d97ba163
Daniel Bryce <dbryce@sift.net>
2024-01-26 19:32:56 +0000

    checkpoint

--- auxiliary_packages/ibex_tests/src/num_constraint_test.cc
+++ auxiliary_packages/ibex_tests/src/num_constraint_test.cc
@@ -5,93 +5,183 @@
 using namespace ibex;
 using namespace std;
 
-NumConstraint func(Array<ExprSymbol> &args, const ExprCtr &expr)
+using std::chrono::duration;
+using std::chrono::duration_cast;
+using std::chrono::high_resolution_clock;
+using std::chrono::milliseconds;
+
+NumConstraint func(const Array<const ExprSymbol> &args, const ExprCtr &expr)
 {
-    return NumConstraint(args, &expr);
+    cout << "size = " << args.size() << endl;
+    switch (args.size())
+    {
+    case 1:
+        return NumConstraint(args[0], expr);
+    case 2:
+        return NumConstraint(args[0], args[1], expr);
+    case 3:
+        return NumConstraint(args[0], args[1], args[2], expr);
+    case 4:
+        return NumConstraint(args[0], args[1], args[2], args[3], expr);
+    case 5:
+        return NumConstraint(args[0], args[1], args[2], args[3], args[4], expr);
+    case 6:
+        return NumConstraint(args[0], args[1], args[2], args[3], args[4], args[5], expr);
+    case 7:
+        return NumConstraint(args[0], args[1], args[2], args[3], args[4], args[5], args[6], expr);
+    case 8:
+        return NumConstraint(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], expr);
+    case 9:
+        return NumConstraint(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], expr);
+    case 10:
+        return NumConstraint(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], expr);
+    case 11:
+        return NumConstraint(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], expr);
+    case 12:
+        return NumConstraint(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11], expr);
+    case 13:
+        return NumConstraint(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11], args[12], expr);
+    case 14:
+        return NumConstraint(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11], args[12], args[13], expr);
+    case 15:
+        return NumConstraint(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11], args[12], args[13], args[14], expr);
+    case 16:
+        return NumConstraint(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11], args[12], args[13], args[14], args[15], expr);
+    case 17:
+        return NumConstraint(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11], args[12], args[13], args[14], args[15], args[16], expr);
+    case 18:
+        return NumConstraint(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11], args[12], args[13], args[14], args[15], args[16], args[17], expr);
+    case 19:
+        return NumConstraint(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11], args[12], args[13], args[14], args[15], args[16], args[17], args[18], expr);
+    case 20:
+        return NumConstraint(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11], args[12], args[13], args[14], args[15], args[16], args[17], args[18], args[19], expr);
+    default:
+        return NumConstraint(args, expr);
+    }
 }
 
-void c1()
+// void c1()
+// {
+//     int num_vars = 20;
+//     Variable x[num_vars];
+
+//     Array<const ExprSymbol> vars(num_vars);
+
+//     const ExprNode *expr = &(x[0] + 1);
+//     for (int i = 1; i < num_vars; i++)
+//         expr = &(*expr + x[i]);
+//     // expr = &(*expr < 0);
+
+//     for (int i = 0; i < num_vars; i++)
+//     {
+//         vars.set_ref(i, x[i]);
+//         // expr = expr + Function(x[i], x[i]);
+//     }
+//     // Function f(vars, *expr, "f");
+
+//     NumConstraint c1 = func(vars, (*expr < 0)); // x[0] + x[1] + x[2] + x[3] + x[4] = 0);
+//     cout << "Made constraint c1: " << c1 << endl;
+// }
+
+// void c2()
+// {
+//     int num_vars = 20;
+
+//     // Function f(vars, *expr, "f");
+
+//     NumConstraint c2(x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7], x[8], x[9], x[10], x[11], x[12], x[13], x[14], x[15], x[16], x[17], x[18], x[19], *expr < 0); // x[0] + x[1] + x[2] + x[3] + x[4] = 0);
+//     cout << "Made constraint c2: " << c2 << endl;
+// }
+
+duration<double, std::milli> make_constraint(const ExprNode *expr, Array<const ExprSymbol> &vars, bool use_opt)
 {
-    using namespace std::chrono_literals;
-    int num_vars = 20;
-    Variable x[num_vars];
+    auto t1 = high_resolution_clock::now();
+    // cout << vars.size() << endl;
+    cout << "done expr" << endl;
 
-    Array<const ExprSymbol> vars(num_vars);
+    NumConstraint c1 = use_opt ? func(vars, (*expr < 0)) : NumConstraint(vars, (*expr < 0));
 
+    cout << "done expr" << endl;
+    auto t2 = high_resolution_clock::now();
+    duration<double, std::milli> ms_double = t2 - t1;
+    return ms_double;
+}
+
+const ExprNode *make_expr(Array<const ExprSymbol> &x)
+{
+
+    int num_vars = x.size();
     const ExprNode *expr = &(x[0] + 1);
     for (int i = 1; i < num_vars; i++)
         expr = &(*expr + x[i]);
-    // expr = &(*expr < 0);
-
-    for (int i = 0; i < num_vars; i++)
-    {
-        vars.set_ref(i, x[i]);
-        // expr = expr + Function(x[i], x[i]);
-    }
-    // Function f(vars, *expr, "f");
-
-    NumConstraint c1(func(&vars, *expr < 0)); // x[0] + x[1] + x[2] + x[3] + x[4] = 0);
-    cout << "Made constraint c1: " << c1 << endl;
+    return expr;
 }
 
-void c2()
+void make_vars(Array<const ExprSymbol> *vars)
 {
-    using namespace std::chrono_literals;
-    int num_vars = 20;
+    int num_vars = vars->size();
     Variable x[num_vars];
-
-    Array<const ExprSymbol> vars(num_vars);
-    const ExprNode *expr = &(x[0] + 1);
-    for (int i = 1; i < num_vars; i++)
-        expr = &(*expr + x[i]);
-    // expr = &(*expr < 0);
+    cout << "num_vars: " << num_vars << endl;
 
     for (int i = 0; i < num_vars; i++)
     {
-        vars.set_ref(i, x[i]);
-        // expr = expr + Function(x[i], x[i]);
+        cout << "i = " << i << endl;
+        vars->set_ref(i, x[i]);
     }
-    // Function f(vars, *expr, "f");
+}
 
-    NumConstraint c2(x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7], x[8], x[9], x[10], x[11], x[12], x[13], x[14], x[15], x[16], x[17], x[18], x[19], *expr < 0); // x[0] + x[1] + x[2] + x[3] + x[4] = 0);
-    cout << "Made constraint c2: " << c2 << endl;
+void summary(int num_vars, double time_with, double time_without)
+{
+    double speedup = time_without / time_with;
+    cout << num_vars << ": " << time_with << " " << time_without << " (" << speedup << "x)" << endl;
 }
 
 int main()
 {
-    Variable x;
-    NumConstraint c(x, x + 1 <= 0);
-    cout << "Made constraint: " << c << endl;
+    // Variable x;
+    // NumConstraint c(x, x + 1 <= 0);
+    // cout << "Made constraint: " << c << endl;
+    cout << "Starting: " << endl;
+    int num_vars = 20;
+    Variable x[num_vars];
+    Array<const ExprSymbol> vars(num_vars);
 
-    using std::chrono::duration;
-    using std::chrono::duration_cast;
-    using std::chrono::high_resolution_clock;
-    using std::chrono::milliseconds;
+    // cout << "Made vars: " << vars.size() << endl;
+    make_vars(&vars);
+    cout << vars[0] << endl;
+    cout << "Made vars: " << vars.size() << endl;
+    const ExprNode *expr = make_expr(vars);
+    cout << "Made expr" << endl;
+    cout << (*expr) << endl;
+    auto time_with = make_constraint(expr, vars, true);
+    auto time_without = make_constraint(expr, vars, false);
+    // summary(vars.size(), time_with.count(), time_without.count());
+    delete expr;
 
-    auto t1 = high_resolution_clock::now();
-    c1();
-    auto t2 = high_resolution_clock::now();
-    c2();
+    // auto t1 = high_resolution_clock::now();
+    // c1();
+    // auto t2 = high_resolution_clock::now();
+    // // c2();
 
-    auto t3 = high_resolution_clock::now();
+    // auto t3 = high_resolution_clock::now();
 
-    /* Getting number of milliseconds as an integer. */
-    auto ms_int = duration_cast<milliseconds>(t2 - t1);
+    // /* Getting number of milliseconds as an integer. */
+    // auto ms_int = duration_cast<milliseconds>(t2 - t1);
 
-    /* Getting number of milliseconds as a double. */
-    duration<double, std::milli> ms_double = t2 - t1;
+    // /* Getting number of milliseconds as a double. */
+    // duration<double, std::milli> ms_double = t2 - t1;
 
-    std::cout << ms_int.count() << "ms\n";
-    std::cout << ms_double.count() << "ms\n";
+    // std::cout << ms_int.count() << "ms\n";
+    // std::cout << ms_double.count() << "ms\n";
 
-    /* Getting number of milliseconds as an integer. */
-    auto ms_int1 = duration_cast<milliseconds>(t3 - t2);
+    // /* Getting number of milliseconds as an integer. */
+    // auto ms_int1 = duration_cast<milliseconds>(t3 - t2);
 
-    /* Getting number of milliseconds as a double. */
-    duration<double, std::milli> ms_double1 = t3 - t2;
+    // /* Getting number of milliseconds as a double. */
+    // duration<double, std::milli> ms_double1 = t3 - t2;
 
-    std::cout << ms_int1.count() << "ms\n";
-    std::cout << ms_double1.count() << "ms\n";
+    // // std::cout << ms_int1.count() << "ms\n";
+    // // std::cout << ms_double1.count() << "ms\n";
 
     return 0;
 }
