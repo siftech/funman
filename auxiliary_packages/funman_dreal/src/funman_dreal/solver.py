b9f3cbf6d537bc46ec5ba67051f3903b76eb6ee2
Dan Bryce <dbryce@sift.net>
2025-04-29 14:06:19 +0000

    cleanup notebooks

--- auxiliary_packages/funman_dreal/src/funman_dreal/solver.py
+++ auxiliary_packages/funman_dreal/src/funman_dreal/solver.py
@@ -442,9 +442,10 @@ class DRealNative(
         # self.context.config.use_worklist_fixpoint = True
         self.model = None
         self.log_level = dreal.LogLevel.OFF
-        self.config.random_seed = 0
+        if hasattr(self.config, "random_seed"):
+            self.config.random_seed = 0
         if "solver_options" in options:
-            if (
+            if (hasattr(self.config, "preferred") and \
                 "preferred" in options["solver_options"]
                 and len(options["solver_options"]["preferred"]) > 0
             ):
@@ -453,7 +454,9 @@ class DRealNative(
                 self.config.precision = options["solver_options"][
                     "dreal_precision"
                 ]
-            if "random_seed" in options["solver_options"]:
+            if hasattr(self.config, "preferred") and \
+                "random_seed" in options["solver_options"]:
+                
                 self.config.random_seed = options["solver_options"][
                     "random_seed"
                 ]
